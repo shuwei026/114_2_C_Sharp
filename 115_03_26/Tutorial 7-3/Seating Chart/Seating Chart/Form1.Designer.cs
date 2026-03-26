04571c7bb1b048085bc7febecac55375ab8de6ea
shuwei026 <A114221026@mail.shu.edu.tw>
2026-03-26 16:04:17 +0800

    115_03_26

---
+++ 115_03_26/Tutorial 7-3/Seating Chart/Seating Chart/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Seating_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.airplanePictureBox = new System.Windows.Forms.PictureBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.seatPriceDescriptionLabel = new System.Windows.Forms.Label();
+            this.colPromptLabel = new System.Windows.Forms.Label();
+            this.rowPromptLabel = new System.Windows.Forms.Label();
+            this.colTextBox = new System.Windows.Forms.TextBox();
+            this.rowTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.displayPriceButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.airplanePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // 注意：以下為繁體中文註解，說明各元件的用途與調整
+            // airplanePictureBox: 顯示飛機座位圖的圖片，置於表單背景左上方
+            // priceLabel: 顯示所選座位價格，初始為空白或顯示「價格」提示
+            // seatPriceDescriptionLabel: 價格說明標籤（繁體中文）
+            // colPromptLabel / rowPromptLabel: 欄/列輸入提示（繁體中文）
+            // colTextBox / rowTextBox: 使用者輸入欄與列的文字方塊
+            // displayPriceButton: 按下後顯示所輸入座位的價格
+            // exitButton: 關閉表單
+            // 為配合 18pt 字型，所有元件都調整了 Size 與 Location，以避免文字被截斷
+            // 
+            // airplanePictureBox
+            //
+            this.airplanePictureBox.Image = global::Seating_Chart.Properties.Resources.Airplane;
+            this.airplanePictureBox.Location = new System.Drawing.Point(0, 0);
+            this.airplanePictureBox.Name = "airplanePictureBox";
+            // 將圖片區域放大以配合較大的字體與元件
+            this.airplanePictureBox.Size = new System.Drawing.Size(520, 640);
+            this.airplanePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.airplanePictureBox.TabIndex = 0;
+            this.airplanePictureBox.TabStop = false;
+            // 
+            // priceLabel
+            //
+            this.priceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.priceLabel.Location = new System.Drawing.Point(360, 14);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(130, 44);
+            this.priceLabel.TabIndex = 21;
+            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 設定為繁體中文提示，實際價格會由程式動態改寫
+            this.priceLabel.Text = "價格";
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // seatPriceDescriptionLabel
+            //
+            this.seatPriceDescriptionLabel.AutoSize = true;
+            this.seatPriceDescriptionLabel.BackColor = System.Drawing.Color.White;
+            this.seatPriceDescriptionLabel.Location = new System.Drawing.Point(260, 24);
+            this.seatPriceDescriptionLabel.Name = "seatPriceDescriptionLabel";
+            this.seatPriceDescriptionLabel.Size = new System.Drawing.Size(94, 30);
+            this.seatPriceDescriptionLabel.TabIndex = 20;
+            // 將文字改為繁體中文「座位價格」並調整字型大小
+            this.seatPriceDescriptionLabel.Text = "座位價格";
+            this.seatPriceDescriptionLabel.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // colPromptLabel
+            //
+            this.colPromptLabel.AutoSize = true;
+            this.colPromptLabel.BackColor = System.Drawing.Color.White;
+            this.colPromptLabel.Location = new System.Drawing.Point(12, 64);
+            this.colPromptLabel.Name = "colPromptLabel";
+            this.colPromptLabel.Size = new System.Drawing.Size(120, 30);
+            this.colPromptLabel.TabIndex = 19;
+            // 將文字改為繁體中文「請輸入欄號'
+            this.colPromptLabel.Text = "請輸入欄號";
+            this.colPromptLabel.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // rowPromptLabel
+            //
+            this.rowPromptLabel.AutoSize = true;
+            this.rowPromptLabel.BackColor = System.Drawing.Color.White;
+            this.rowPromptLabel.Location = new System.Drawing.Point(12, 16);
+            this.rowPromptLabel.Name = "rowPromptLabel";
+            this.rowPromptLabel.Size = new System.Drawing.Size(120, 30);
+            this.rowPromptLabel.TabIndex = 18;
+            // 將文字改為繁體中文「請輸入列號
+            this.rowPromptLabel.Text = "請輸入列號";
+            this.rowPromptLabel.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // colTextBox
+            //
+            this.colTextBox.Location = new System.Drawing.Point(140, 64);
+            this.colTextBox.Name = "colTextBox";
+            // 調整文字方塊高度以配合 18pt 字型
+            this.colTextBox.Size = new System.Drawing.Size(100, 39);
+            this.colTextBox.TabIndex = 17;
+            this.colTextBox.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // rowTextBox
+            //
+            this.rowTextBox.Location = new System.Drawing.Point(140, 16);
+            this.rowTextBox.Name = "rowTextBox";
+            this.rowTextBox.Size = new System.Drawing.Size(100, 39);
+            this.rowTextBox.TabIndex = 16;
+            this.rowTextBox.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            // 
+            // exitButton
+            //
+            this.exitButton.Location = new System.Drawing.Point(260, 660);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 44);
+            this.exitButton.TabIndex = 23;
+            // 按鈕文字改為繁體中文「離開」
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // displayPriceButton
+            //
+            this.displayPriceButton.Location = new System.Drawing.Point(140, 660);
+            this.displayPriceButton.Name = "displayPriceButton";
+            this.displayPriceButton.Size = new System.Drawing.Size(120, 44);
+            this.displayPriceButton.TabIndex = 22;
+            // 按鈕文字改為繁體中文「顯示價格」
+            this.displayPriceButton.Text = "顯示價格";
+            this.displayPriceButton.UseVisualStyleBackColor = true;
+            this.displayPriceButton.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.displayPriceButton.Click += new System.EventHandler(this.displayPriceButton_Click);
+            // 
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // 調整表單大小以配合放大的元件
+            this.ClientSize = new System.Drawing.Size(520, 720);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayPriceButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.seatPriceDescriptionLabel);
+            this.Controls.Add(this.colPromptLabel);
+            this.Controls.Add(this.rowPromptLabel);
+            this.Controls.Add(this.colTextBox);
+            this.Controls.Add(this.rowTextBox);
+            this.Controls.Add(this.airplanePictureBox);
+            this.Name = "Form1";
+            // 表單標題改為繁體中文「座位配置
+            this.Text = "座位配置";
+            ((System.ComponentModel.ISupportInitialize)(this.airplanePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox airplanePictureBox;
+        internal System.Windows.Forms.Label priceLabel;
+        internal System.Windows.Forms.Label seatPriceDescriptionLabel;
+        internal System.Windows.Forms.Label colPromptLabel;
+        internal System.Windows.Forms.Label rowPromptLabel;
+        internal System.Windows.Forms.TextBox colTextBox;
+        internal System.Windows.Forms.TextBox rowTextBox;
+        internal System.Windows.Forms.Button exitButton;
+        internal System.Windows.Forms.Button displayPriceButton;
+    }
+}
+
---
+++ 115_03_26/Tutorial 7-4/Test Score List/Test Score List/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Test_Score_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testScoresListBox = new System.Windows.Forms.ListBox();
+            this.avgScoreDescriptionLabel = new System.Windows.Forms.Label();
+            this.aboveAvgDescriptionLabel = new System.Windows.Forms.Label();
+            this.belowAvgDescriptionLabel = new System.Windows.Forms.Label();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.aboveAverageLabel = new System.Windows.Forms.Label();
+            this.belowAverageLabel = new System.Windows.Forms.Label();
+            this.getScoresButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testScoresListBox
+            // 
+            this.testScoresListBox.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.testScoresListBox.FormattingEnabled = true;
+            this.testScoresListBox.ItemHeight = 45;
+            this.testScoresListBox.Location = new System.Drawing.Point(18, 17);
+            this.testScoresListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.testScoresListBox.Name = "testScoresListBox";
+            this.testScoresListBox.Size = new System.Drawing.Size(298, 274);
+            this.testScoresListBox.TabIndex = 0;
+            // 
+            // avgScoreDescriptionLabel
+            // 
+            this.avgScoreDescriptionLabel.AutoSize = true;
+            this.avgScoreDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.avgScoreDescriptionLabel.Location = new System.Drawing.Point(345, 28);
+            this.avgScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.avgScoreDescriptionLabel.Name = "avgScoreDescriptionLabel";
+            this.avgScoreDescriptionLabel.Size = new System.Drawing.Size(200, 45);
+            this.avgScoreDescriptionLabel.TabIndex = 1;
+            this.avgScoreDescriptionLabel.Text = "平均分數：";
+            // 
+            // aboveAvgDescriptionLabel
+            // 
+            this.aboveAvgDescriptionLabel.AutoSize = true;
+            this.aboveAvgDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.aboveAvgDescriptionLabel.Location = new System.Drawing.Point(345, 97);
+            this.aboveAvgDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.aboveAvgDescriptionLabel.Name = "aboveAvgDescriptionLabel";
+            this.aboveAvgDescriptionLabel.Size = new System.Drawing.Size(380, 45);
+            this.aboveAvgDescriptionLabel.TabIndex = 2;
+            this.aboveAvgDescriptionLabel.Text = "高於平均分數的數量：";
+            // 
+            // belowAvgDescriptionLabel
+            // 
+            this.belowAvgDescriptionLabel.AutoSize = true;
+            this.belowAvgDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.belowAvgDescriptionLabel.Location = new System.Drawing.Point(345, 166);
+            this.belowAvgDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.belowAvgDescriptionLabel.Name = "belowAvgDescriptionLabel";
+            this.belowAvgDescriptionLabel.Size = new System.Drawing.Size(380, 45);
+            this.belowAvgDescriptionLabel.TabIndex = 3;
+            this.belowAvgDescriptionLabel.Text = "低於平均分數的數量：";
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.averageLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.averageLabel.Location = new System.Drawing.Point(630, 22);
+            this.averageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(179, 49);
+            this.averageLabel.TabIndex = 4;
+            this.averageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // aboveAverageLabel
+            // 
+            this.aboveAverageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.aboveAverageLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.aboveAverageLabel.Location = new System.Drawing.Point(630, 91);
+            this.aboveAverageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.aboveAverageLabel.Name = "aboveAverageLabel";
+            this.aboveAverageLabel.Size = new System.Drawing.Size(179, 49);
+            this.aboveAverageLabel.TabIndex = 5;
+            this.aboveAverageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // belowAverageLabel
+            // 
+            this.belowAverageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.belowAverageLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.belowAverageLabel.Location = new System.Drawing.Point(630, 161);
+            this.belowAverageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.belowAverageLabel.Name = "belowAverageLabel";
+            this.belowAverageLabel.Size = new System.Drawing.Size(179, 49);
+            this.belowAverageLabel.TabIndex = 6;
+            this.belowAverageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // getScoresButton
+            // 
+            this.getScoresButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.getScoresButton.Location = new System.Drawing.Point(345, 277);
+            this.getScoresButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getScoresButton.Name = "getScoresButton";
+            this.getScoresButton.Size = new System.Drawing.Size(240, 62);
+            this.getScoresButton.TabIndex = 7;
+            this.getScoresButton.Text = "取得成績";
+            this.getScoresButton.UseVisualStyleBackColor = true;
+            this.getScoresButton.Click += new System.EventHandler(this.getScoresButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Location = new System.Drawing.Point(615, 277);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(195, 62);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.searchLabel.Location = new System.Drawing.Point(-5, 440);
+            this.searchLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(200, 45);
+            this.searchLabel.TabIndex = 9;
+            this.searchLabel.Text = "搜尋成績：";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.searchTextBox.Location = new System.Drawing.Point(187, 430);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(178, 55);
+            this.searchTextBox.TabIndex = 10;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.searchButton.Location = new System.Drawing.Point(395, 423);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(150, 62);
+            this.searchButton.TabIndex = 11;
+            this.searchButton.Text = "搜尋";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchResultLabel
+            // 
+            this.searchResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchResultLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.searchResultLabel.Location = new System.Drawing.Point(576, 423);
+            this.searchResultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.searchResultLabel.Name = "searchResultLabel";
+            this.searchResultLabel.Size = new System.Drawing.Size(254, 62);
+            this.searchResultLabel.TabIndex = 12;
+            this.searchResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 547);
+            this.Controls.Add(this.searchResultLabel);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getScoresButton);
+            this.Controls.Add(this.belowAverageLabel);
+            this.Controls.Add(this.aboveAverageLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.belowAvgDescriptionLabel);
+            this.Controls.Add(this.aboveAvgDescriptionLabel);
+            this.Controls.Add(this.avgScoreDescriptionLabel);
+            this.Controls.Add(this.testScoresListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "測驗成績清單";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox testScoresListBox;
+        private System.Windows.Forms.Label avgScoreDescriptionLabel;
+        private System.Windows.Forms.Label aboveAvgDescriptionLabel;
+        private System.Windows.Forms.Label belowAvgDescriptionLabel;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label aboveAverageLabel;
+        private System.Windows.Forms.Label belowAverageLabel;
+        private System.Windows.Forms.Button getScoresButton;
+        private System.Windows.Forms.Button exitButton;
+        // 新增：搜尋相關欄位宣告
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label searchResultLabel;
+    }
+}
+
